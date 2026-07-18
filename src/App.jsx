9e3e9cb202eb673b72ed2fc9359ff021e0104b04
BOSTON\dburgos <dburgos@spa.com>
2026-07-18 16:05:32 -0400

    Added authentication setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Checkout from "./pages/Checkout";
 import Navbar from './components/Navbar';
 
 import './App.css'
+import AuthProvider from './context/AuthContext';
 
 function App() {
 
   return (
+    <AuthProvider>
     <div className="app">
       <Navbar />
       <Routes>
@@ -18,6 +20,7 @@ function App() {
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
+    </AuthProvider>
   );
 }
 
