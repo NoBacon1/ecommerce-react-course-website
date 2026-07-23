52af293f72df07be14307cd5347616303e4df29e
BOSTON\dburgos <dburgos@spa.com>
2026-07-23 16:18:02 -0400

    Added the Checkout page functionaility

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,17 @@
 {/* This script pulled the product card set up in Home into its own component to be used whenever we need it */ }
 import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 export default function ProductCard({ product }) {
+    const { addToCart, cartItems } = useCart();
+    const productInCart = product
+    ? cartItems.find((item) => item.id === product.id)
+    : null;
+
+    const productQuanityLabel = productInCart 
+    ? `(${productInCart.quantity})`
+    : "";
+
     return (
         <div className="product-card">
             <img src={product.image} alt={product.name} className="product-card-image" />
@@ -12,7 +22,12 @@ export default function ProductCard({ product }) {
                     <Link className="btn btn-secondary" to={`/products/${product.id}`}>
                         View Details
                     </Link>
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => addToCart(product.id)}
+                    >
+                        Add to Cart {productQuanityLabel}
+                    </button>
                 </div>
             </div>
         </div>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,12 +2,21 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductById } from "../data/products";
+import { useCart } from "../context/CartContext";
 
 
 export default function ProductDetails() {
     const { id } = useParams();
     const [product, setProduct] = useState(null); {/* Create a state to keep track of what product we want to display */ }
     const navigate = useNavigate();
+    const { addToCart, cartItems } = useCart();
+    const productInCart = product
+        ? cartItems.find((item) => item.id === product.id)
+        : null;
+
+    const productQuantityLabel = productInCart
+        ? `(${productInCart.quantity})`
+        : "";
 
     useEffect(() => {
         const foundProduct = getProductById(id);
@@ -36,7 +45,12 @@ export default function ProductDetails() {
                         <h1 className="product-detail-name">{product.name}</h1>
                         <p className="product-detail-price">${product.price}</p>
                         <p className="product-detail-description">{product.description}</p>
-                        <button className="btn btn-primary">Add to Cart</button>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => addToCart(product.id)}
+                        >
+                            Add to Cart {productQuantityLabel}
+                        </button>
                     </div>
                 </div>
             </div>
